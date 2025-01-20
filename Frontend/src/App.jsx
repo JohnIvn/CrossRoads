eb3691731ada9f2033b9103d406fd40990523b2e
Matthew Cania <139668141+MattCania@users.noreply.github.com>
2025-01-20 16:27:08 +0800

    frontend packages setup

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+      <div
+        className='flex justify-center items-center w-screen h-screen'
+      >
+        <h1
+          className='text-black text-4xl font-bold'
+        >Hello</h1>
+
+      </div>
+  )
+}
+
+export default App
